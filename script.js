264d7733d1ed84ec61dbfb60a04f6c45e303cf16
vnw7wgfwzc-create <vnw7wgfwzc@privaterelay.appleid.com>
2026-07-10 14:51:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("RYO AI");
+async function loadNews(){
+
+const res = await fetch("news.json");
+
+const data = await res.json();
+
+document.getElementById("summary").innerHTML =
+data.summary.join("<br>");
+
+document.getElementById("updated").innerHTML =
+"最終更新 " + new Date().toLocaleString();
+
+}
+
+loadNews();
